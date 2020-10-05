4086931bc24eb9cda68c247cc648f69079e65027
Rishit <rishitshivesh@gmail.com>
2020-10-05 18:56:20 +0530

    specific char now

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -7,11 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     $(function() {
         $('.space').on('keypress', function(e) {
-            if (e.which == 32) {
-                console.log('Space Not Allowed');
-                return false;
-            } else if ((e.which < 48 || e.which >= 57) || (e.which <= 65 && e.which >= 122)) {
-                console.log(`Character ${e.which} not allowed`);
+            if (e.which < 48 || e.which == 58 || e.which == 59 || e.which == 60 || e.which == 61 || e.which == 62 || e.which == 63 || e.which == 64 || e.which == 91 || e.which == 92 || e.which == 93 || e.which == 94 || e.which == 95 || e.which == 96 || e.which > 122) {
                 return false;
             }
         });
