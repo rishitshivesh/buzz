8b12abf33dd30a42cffaf652bd73b760d77d5435
Rishit <rishitshivesh@gmail.com>
2020-10-05 19:04:05 +0530

    specific char now yes

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     $(function() {
         $('.space').on('keypress', function(e) {
-            if (e.which < 46 || e.which == 58 || e.which == 59 || e.which == 60 || e.which == 61 || e.which == 62 || e.which == 63 || e.which == 64 || e.which == 91 || e.which == 92 || e.which == 93 || e.which == 94 || e.which == 95 || e.which == 96 || e.which > 122) {
+            if (e.which < 46 || e.which == 47 || e.which == 58 || e.which == 59 || e.which == 60 || e.which == 61 || e.which == 62 || e.which == 63 || e.which == 64 || e.which == 91 || e.which == 92 || e.which == 93 || e.which == 94 || e.which == 96 || e.which > 122) {
                 return false;
             }
         });
